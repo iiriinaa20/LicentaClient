a3b0ccf9042a3797428903f2642dd77cd463ba8a
Irina Mihaela <irinamihaela.calota@ulbsibiu.ro>
2024-07-05 06:18:33 +0300

    commit 4

---
+++ android/app/src/main/kotlin/com/example/ceta/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ceta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
+
+//package com.example.ceta
+
+//import io.flutter.embedding.engine.FlutterEngine;
+////import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+
+//public class MainActivity extends FlutterActivity {
+//    @Override
+//    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
+//}
